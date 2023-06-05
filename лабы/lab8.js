d7f0e4cce2d425177657c4859d7184cbe03c84fb
Юлия Яцынина <julisa.milina@gmail.com>
2023-06-05 21:36:45 +0700

    Время

--- лабы/lab8.js
+++ лабы/lab8.js
@@ -26,3 +26,9 @@ function detet(){
     document.getElementById('rojd').innerHTML="";
 }
 
+function showTime(){
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime
+}
+setInterval(showTime, 1000);
